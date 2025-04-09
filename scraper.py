4f095a4164f1071b360b11b1d18e01ca6f46c77a
DukeLetholdus <leeuwrobin@hotmail.nl>
2025-04-09 15:27:34 +0300

    .

--- scraper.py
+++ scraper.py
@@ -3,18 +3,19 @@ import csv
 with open('./logs/internetspeed.csv') as f:
     reader = csv.reader(f)
     loglist = list(reader)
-    print(loglist)
 
-#average download
+# average download
 counter = 0
 total = 0
 for i in range(len(loglist)):
     total += int(loglist[i][3])
     counter += 1
 
-print("Total measurements: " + str(counter))
-print("Average download: " + str(round(total/counter)))
+with open("./logs/output.txt", "w") as text_file:
+    print("Total measurements: {counter}".format(counter=counter), file=text_file)
+    print("Average download: {average} mbps".format(average=round(total/counter, 1)), file=text_file)
 
+# calculate percentage internet bad
 slow = 0
 fast = 0
 threshold = 15
@@ -24,5 +25,6 @@ for i in range(len(loglist)):
         slow += 1
     else:
         fast += 1
-downtime = slow/(slow + fast)*100
-print("Downtime is: " + str(downtime))
+downtime = round(slow/(slow + fast)*150, 1)
+with open("./logs/output.txt", "a") as text_file:
+    print("Downtime is: {downtime}%".format(downtime=downtime), file=text_file)
